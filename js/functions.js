3265a74bbc362216db3a6dd1bd242ad58d1e2594
naranjito72 <theclick36@gmail.com>
2022-02-03 16:35:13 +0100

    day one

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,19 @@
+// functions js
 function helloMe() {
     const hello = 'Hello World';
     console.log(hello);
     return hello;
 }
-const hello = () => 'Hello World';
+const hello = () => console.log('Hello World');
+// functions with params
+const sum = (num1, num2) => num1 + num2;
+console.log(sum(895, 7589));
+const timesFive = (num) => num * 5;
+console.log(timesFive(5));
+// scopes
+const GLOBAL = 10;
+let globalOne = 10;
+function scopeAll() {
+    // allScope = 5;
+    // error in typescript
+}
