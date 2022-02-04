87edf7a96cd55811e3c944efee22435a8eec6532
naranjito72 <theclick36@gmail.com>
2022-02-04 10:54:31 +0100

    installing jasmine

--- src/strings.js
+++ src/strings.js
@@ -3,6 +3,5 @@ const myNoun = "dog";
 const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
-
 const wordBlanks = "The" + " " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " " + "behind the bushes.";
 console.log(wordBlanks);
